73ac163692ce309ae51948a8b7b6819e08bb97e9
Nino Dellomas <ninodellomas@hotmail.com>
2023-11-28 14:40:05 +0800

    fix about.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -32,49 +32,17 @@ const About = () => {
           <div className="w-ful flex flex-col justify-center items-center">
           <div className="text-center space-y-[20px]">
             <h1 className="text-primary text-[32px]"> Professional Experience</h1>
-            <h2>Music Teacher (Full Time)</h2>
-            <p>The Catholic Board of Education Bahamas | November 2022 - Present</p>
-                 <p>Playing a pivotal role in designing and implementing a comprehensive music curriculum that aligns with educational standards and caters to the diverse needs of students across different grade levels.</p>
-                 <p>Crafting engaging lesson plans that incorporate a variety of musical genres, instruments, and interactive activities to create a dynamic and enriching learning experience.</p>
-                 <p>Implementing strategies to accommodate diverse learning styles, allows each student to actively participate and fi nd joy in the exploration of music.</p>
-                 <p>Working towards integrating music into the broader school curriculum, fostering interdisciplinary connections and enhancing the overall educational experience.</p>
-                 <p>Actively involved in organizing and directing various extracurricular musical activities, including school concerts, talent shows, and choir performances.</p>
-                 <p>Won the National Arts Festivals with Distinction Award in the Gospel Choir Category, and Merit and Credit Awards in Solo and Duet Singing Categories.</p>
+            <p>Music Teacher (Full Time)</p>
+            <h3>The Catholic Board of Education Bahamas | November 2022 - Present</h3>
+                 <h3>Playing a pivotal role in designing and implementing a comprehensive music curriculum that aligns with educational standards and caters to the diverse needs of students across different grade levels.</h3>
+                 <h3>Crafting engaging lesson plans that incorporate a variety of musical genres, instruments, and interactive activities to create a dynamic and enriching learning experience.</h3>
+                 <h3>Implementing strategies to accommodate diverse learning styles, allows each student to actively participate and fi nd joy in the exploration of music.</h3>
+                 <h3>Working towards integrating music into the broader school curriculum, fostering interdisciplinary connections and enhancing the overall educational experience.</h3>
+                 <h3>Actively involved in organizing and directing various extracurricular musical activities, including school concerts, talent shows, and choir performances.</h3>
+                 <h3>Won the National Arts Festivals with Distinction Award in the Gospel Choir Category, and Merit and Credit Awards in Solo and Duet Singing Categories.</h3>
              
-                      <br></br>
-                <h2>Music, Arts, Physical Education, and Health Teacher (Full Time)</h2>
-                <p>Department of Education/ Barcelona National Comprehensive High School/Philippines | August 2019 - December 2022</p>
+                     
               
-                <p>Actively participated in the design and development of the music curriculum, aligning  it with national educational standards and regional needs.</p>
-                <p>Advocated for the incorporation of innovative teaching methodologies and technology within the music curriculum.</p>
-                <p>Worked collaboratively with fellow curriculum writers to ensure that the music curriculum aligned with broader educational goals and objectives set by the Department of Education.</p>
-                <p>Participated in professional development initiatives within the Music Curriculum Writers team, staying abreast of the latest trends, research, and best practices in musiceducation.</p>
-                <p>Actively contributed to the creation of video lessons for the DepEd Sorsogon Division Television Channel.</p>
-                <p>Conducted training sessions for the Teachers Choir of the Public School Schools District of Barcelona, Sorsogon, Philippines.</p>
-                <p>Trained and led a choral group, participating in various stage performances and competitions.</p>
-                <p>Provided training to local musicians, contributing to the development of musical skills within the community.</p>
-                <p>Actively participated as a Guitar and Bass Player in the Bicol Regional Teachers Rondalla under DepEd Region V.</p>
-                <p>Trained and conducted a Rondalla ensemble, showcasing traditional Filipino string ensemble music.</p>
-                <p>Engaged in various regional and national cultural activities, contributing to the promotion of Filipino arts and cultural identity.</p>
-                 
-                      <br></br>
-                <h2>Music, Arts, Physical Education, and Health Teacher (Full Time)</h2>
-                <p>St. Louise de Marillac College of Sorsogon Inc./Philippines | June 2017- April 2019</p>
-              
-                <p>Meticulously aligned lessons with the prescribed Department of Education curriculum, ensuring that each activity and objective is in harmony with educational standards.</p>
-                <p>Implemented differentiated instruction to cater to diverse learning styles and abilities within the classroom</p>
-                <p>Utilized a variety of instructional techniques, such as multimedia presentations,interactive activities, and real-world applications, to make the learning experience more captivating and relevant to students.</p>
-                <p>Successfully trained and led a choral group, showcasing their talents through various stage performances and concerts.</p>
-                <p>Took on the responsibility of training and leading a School Drum and Lyre Corps, ensuring their participation in various school events and province-wide performances.</p>
-                <p>Coached and trained a group of students and teachers, guiding them to an impressive Second Place victory in a Division-Wide Cultural Contest.</p>
-                <p>Collaborated in co-directing a school benefi t concert that garnered significant success, raising more than twenty thousand US dollars ($20,000).</p>
-                                      
-               
-               <br></br>
-               <br></br>
-               <br></br>
-               <br></br>
-               <br></br>
                 </div>
               </div>
             </div>
